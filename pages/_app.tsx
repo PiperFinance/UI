2964b148cc96124d47de4a0d2f20639e68796767
Mapoofano <majidhasanpour3@gmail.com>
2022-11-10 19:54:48 +0330

    [#Feature] connect two wallets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,31 @@ import {
 } from '@rainbow-me/rainbowkit/wallets';
 import { phantom } from '../wallets/phantom';
 
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  LedgerWalletAdapter,
+  MathWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  UnsafeBurnerWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import { useMemo } from 'react';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
 const solanaChain: Chain = {
   id: 103,
   name: 'Solana',
@@ -43,6 +68,25 @@ const solanaChain: Chain = {
   },
   blockExplorers: {
     default: { name: 'Solscan', url: 'https://solscan.io/' },
+  },
+  testnet: false,
+};
+
+const tronChain: any = {
+  name: 'Tron',
+  network: 'tron',
+  iconUrl: 'https://example.com/icon.svg',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Tron',
+    symbol: 'TRX',
+  },
+  rpcUrls: {
+    default: 'https://api.mainnet-beta.solana.com',
+  },
+  blockExplorers: {
+    default: { name: 'tronscan', url: 'https://tronscan.org/#/' },
   },
   testnet: false,
 };
@@ -87,9 +131,34 @@ const wagmiClient = createClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+      new LedgerWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new PhantomWalletAdapter(),
+      new MathWalletAdapter(),
+    ],
+    [network]
+  );
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains} modalSize="compact">
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+              {/* Your app's components go here, nested within the context providers. */}
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
         <ERC20Balance />
         <Component {...pageProps} />
       </RainbowKitProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
 import Default from '../components/layout/Default';
-
+import { useSignMessage } from 'wagmi';
+import { useEffect } from 'react';
 export default function Home() {
+  // const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+  //   message: 'gm wagmi frens',
+  // })
+
+  // useEffect(() =>{
+  //   signMessage();
+  // }, [])
+
+  // useEffect(() =>{
+  //   console.log(data);
+  //   console.log(first)
+  // }, [data, isSuccess])
+
   return <Default pageName="Home">{/* <ConnectButton /> */}</Default>;
 }
