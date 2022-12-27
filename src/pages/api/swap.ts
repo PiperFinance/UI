b23fee1a0943346c45cb7632f1918ba32e87161b
Mapoofano <majidhasanpour3@gmail.com>
2022-12-27 18:39:37 +0330

    remove prisma import and primas file

--- src/pages/api/swap.ts
+++ src/pages/api/swap.ts
@@ -1,9 +1,6 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-import { prisma } from "../../server/db/client";
-
 const swap = async (req: NextApiRequest, res: NextApiResponse) => {
-  const swap = await prisma.example.findMany();
   res.status(200).json(swap);
 };
 
